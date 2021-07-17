50a99240f5eef07ccedac42fb358fedafee24d7b
Pierre LEVEQUE <pleveque@e3r7p19.42.fr>
2021-07-17 16:33:17 +0200

    init commit

---
+++ dict_struct.h
@@ -0,0 +1,10 @@
+#ifndef DICT_STRUCT_H
+# define DICT_STRUCT_H
+
+typedef struct s_dict
+{
+	char	*name;
+	char	*value;
+}	t_dict;
+
+#endif
---
+++ ft_h.c
@@ -0,0 +1,6 @@
+#ifndef FT_H
+# define FT_H
+
+#include "dict_struct.h"
+
+#endif
